1ded4a7ada1d6e6f3717e880de758b6e5cab6320
Kidchumaiork <glaosad5@gmail.com>
2024-07-25 00:29:15 +0700

    Update 3 part done
    int, float, string done
    template maximum() need to fix
    
    *not printing the correct way*
    -delete return or delete printing part or change printing part to main()

--- Srisoongnern_hw6.cpp
+++ Srisoongnern_hw6.cpp
@@ -10,10 +10,12 @@ T maximum(const T &a, const T &b)
     {
         auto M = (a < b) ? b : a; // check if a < b is True then return b, but if False then return a
         cout << M << " is larger" << '\n';
+        return M;
     }
     else
     {
         cout << "Both values are equal." << '\n';
+        return a; // or return b since a == b
     }
 }
 
@@ -21,7 +23,18 @@ int main()
 {
     // int part
     int x1 = 0, x2 = 1;
-    cout << "Comparing both values " << maximum(x1, x2);
+    cout << "After comparing both values " << '\n'
+         << maximum(x1, x2);
+
+    // float part
+    double y1 = 5.5, y2 = 5.5;
+    cout << "After comparing both values " << '\n'
+         << maximum(y1, y2);
+
+    // string part
+    string z1 = "marshmallow", z2 = "Hello World!";
+    cout << " After comparing both values " << '\n'
+         << maximum(z1, z2);
 
     return 0;
 }
