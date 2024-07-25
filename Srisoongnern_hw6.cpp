748af622bff5ada5575170eb0448ea847556527d
Kidchumaiork <glaosad5@gmail.com>
2024-07-25 17:28:53 +0700

    Finished
    every part done

--- Srisoongnern_hw6.cpp
+++ Srisoongnern_hw6.cpp
@@ -1,21 +1,35 @@
-// Prompt
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-template <class T> // create template and function 'maximum' which will compare a and b then find which one is larger
-T maximum(const T &a, const T &b)
+template <typename T>
+
+void maximum(const T &a, const T &b)
 {
-    if (a != b)
-    {
-        auto M = (a < b) ? b : a; // check if a < b is True then return b, but if False then return a
-        cout << M << " is larger" << '\n';
-        return M;
+    if constexpr (is_same_v<T, string>)
+    { // Check if the type T is a string
+        if (a.length() != b.length())
+        {
+            const T &value = (a.length() < b.length()) ? b : a;
+            cout << value << " has more characters.\n";
+        }
+        else
+        {
+            cout << "Both values have equal length.\n";
+        }
     }
     else
     {
-        cout << "Both values are equal." << '\n';
-        return a; // or return b since a == b
+        const T &value = (a < b) ? b : a;
+        if (a != b)
+        {
+            cout << value << " is bigger.\n";
+        }
+        else
+        {
+            cout << "Both values are equal.\n";
+        }
     }
 }
 
@@ -23,18 +37,18 @@ int main()
 {
     // int part
     int x1 = 0, x2 = 1;
-    cout << "After comparing both values " << '\n'
-         << maximum(x1, x2);
+    cout << "After comparing both values: ";
+    maximum(x1, x2);
 
     // float part
     double y1 = 5.5, y2 = 5.5;
-    cout << "After comparing both values " << '\n'
-         << maximum(y1, y2);
+    cout << "After comparing both values: ";
+    maximum(y1, y2);
 
     // string part
-    string z1 = "marshmallow", z2 = "Hello World!";
-    cout << " After comparing both values " << '\n'
-         << maximum(z1, z2);
+    string z1 = "Iron Man", z2 = "Spiderman";
+    cout << "After comparing both values: ";
+    maximum(z1, z2);
 
     return 0;
 }
